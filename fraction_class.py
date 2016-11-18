c22c74e85ceb6b0bba045482a40b042218e6ad79
meganduffy <meganemilyduffy@gmail.com>
2016-11-18 16:32:29 +0000

    Floats can be used as input and converted into fractions

--- fraction_class.py
+++ fraction_class.py
@@ -1,76 +1,56 @@
 from fractions import Fraction, gcd
 
-class Fraction_class:
+
+class FractionClass:
     def __init__(self, numerator, denominator):
-        if numerator == float or denominator == float:
-            new = Fraction(self.num).limit_denominator()
-            self.num = int(str(new)[0])
-            self.den = int(str(new)[2])
-
-        self.num = numerator
-        self.den = denominator
+        if float in [type(numerator), type(denominator)]:
+            self.num = Fraction(numerator).limit_denominator()
+            self.den = Fraction(denominator).limit_denominator()
+        else:
+            self.num = numerator
+            self.den = denominator
         self.mod = divmod(self.num, self.den)
-
 
     def __repr__(self, num, den):
         return "%s %s/%s" % (self.mod[0], self.mod[1], self.den)
 
     def __add__(self, other):
-        num = self.num * other.den + self.den * other.num
+        num = (self.num * other.den) + (self.den * other.num)
         den = self.den * other.den
-        if num < den:
-            return Fraction(num, den)
-        else:
-            return "%s %s" % (self.mod[0], Fraction(self.mod[1], self.den))
+        return Fraction(num, den)
 
     def __sub__(self, other):
         num = self.num * other.den - self.den * other.num
         den = self.den * other.den
-        if num < den:
-            return Fraction(num, den)
-        else:
-            return "%s %s" % (self.mod[0], Fraction(self.mod[1], self.den))
+        return Fraction(num, den)
 
     def __mul__(self, other):
         return Fraction(self.num * other.num, self.den * other.den)
+
     def __div__(self, other):
         return Fraction(self.num * other.den, self.den * other.num)
 
-
     def __eq__(self, other):
-        if Fraction(self.num, self.den) == Fraction(other.num, other.den):
-            return "True"
-        else:
-            return "False"
+        return Fraction(self.num, self.den) == Fraction(other.num, other.den)
+
     def __ne__(self, other):
-        if Fraction(self.num, self.den) != Fraction(other.num, other.den):
-            return "True"
-        else:
-            return "False"
+        return Fraction(self.num, self.den) != Fraction(other.num, other.den)
+
     def __lt__(self, other):
-        if Fraction(self.num, self.den) < Fraction(other.num, other.den):
-            return "True"
-        else:
-            return "False"
+        return Fraction(self.num, self.den) < Fraction(other.num, other.den)
+
     def __le__(self, other):
-        if Fraction(self.num, self.den) <= Fraction(other.num, other.den):
-            return "True"
-        else:
-            return "False"
+        return Fraction(self.num, self.den) <= Fraction(other.num, other.den)
+
     def __gt__(self, other):
-        if Fraction(self.num, self.den) > Fraction(other.num, other.den):
-            return "True"
-        else:
-            return "False"
+        return Fraction(self.num, self.den) > Fraction(other.num, other.den)
+
     def __ge__(self, other):
-        if Fraction(self.num, self.den) >= Fraction(other.num, other.den):
-            return "True"
-        else:
-            return "False"
+        return Fraction(self.num, self.den) >= Fraction(other.num, other.den)
 
 
-a = Fraction_class(0.4, 1)
-b = Fraction_class(1, 3)
+a = FractionClass(4, 1)
+b = FractionClass(1, 3)
 f1 = a + b
 f2 = a - b
 f3 = a * b
@@ -82,4 +62,10 @@ f8 = a <= b
 f9 = a > b
 f10 = a >= b
 
-print f1
+assert not f5
+assert f6
+assert not f8
+assert f9
+assert f10
+print "All assertions passed."
+
--- fractions_practice.py
+++ fractions_practice.py
@@ -1,9 +1,19 @@
-import fractions
+from fractions import Fraction
+# a = 11
+# b = 8
+# c = 0.2
+#
+# new = Fraction(c).limit_denominator()
+# print new
+# # print int(str(new)[2])
+# #
+# print Fraction(Fraction(1,2), Fraction(3,4))
+#
+# # def num_or_den(num, den):
+# #     if float in [type(num) or type(den)]:
+# #         frac =
 
-a = 11
-b = 8
-c = 0.2
+a = 1.225
+b = Fraction(1, 2)
 
-new = fractions.Fraction(c).limit_denominator()
-print new
-print int(str(new)[2])
+print a = b
